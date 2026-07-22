46700702082b6f6b88516ae5d3ebbd01aa13156e
Commie45 <rithik.karadiyaan@icloud.com>
2026-07-22 08:07:21 +0530

    Added The Current Code

---
+++ Arduino.cpp
@@ -0,0 +1,42 @@
+//Pins
+
+//Functions
+
+void setup() {
+
+
+}
+
+void loop() {
+  //LDR loop for detecting change in light levels to figure out if an intruder has arrived, return a int val
+  //Humidity loop for detecting a change in humidity, return a int val
+  //Temperature loop for detecting a change in temperature, return a int val
+
+  //Checker function to analyse wheter each of the preceding functions gave an abnormal value, if they did it will make a buzz(unique one) and a light to go off in a repeated loop that will only terminate once a button is pressed
+
+}
+
+//LDR loop 
+void LDR_Monitor(){
+
+}
+
+//Humidity loop
+void Humidity_Monitor(){
+
+}
+
+//Temperature loop
+void Temperature_Monitor(){
+
+}
+
+//Lights Function
+void Lights_Display(){
+
+}
+
+//Buzzer Function
+void 
+
+//Checker function

3e754814bcbfbb81a419bc8d2c6d5a268c640f49
Commie45 <rithik.karadiyaan@icloud.com>
2026-07-23 07:58:18 +0530

    Added descriptions

--- project.ino
+++ project.ino
@@ -1,15 +1,19 @@
-// Abel, P2603281, DCPE/FT/1B/22, Introduction to Engineering Programming
-//Rithik, P263, DCPE/FT/1B/22, Introduction to Engineering Programming
+// Abel(SakamataChloe2), P2603281, DCPE/FT/1B/22, Introduction to Engineering Programming
+// Rithik(Commie_Debug), P263, DCPE/FT/1B/22, Introduction to Engineering Programming
 
 //Description:
-//
-//
-//
-//
-//
-//
-//
-//
+//3 Monitor/Input Functions that will run at the start of setup(). 
+// Each of the functions checks a unique parameter (light levels, humidity, or temperature). T
+// hey will take a reading and process it to see if it is high, low or normal. If it is low it will return a value of 1, if it is high it will return a value of 2, otherwise it returns a value of 0. 
+// The exception is the LDR_Monitor which will only output a value of 1(Abnormal) or 0(Normal).
+
+//1 Computational Function that will have the 3 outputs of the monitor functions will be its arguments.
+// If any of the variables are 1 or 2 (or only 1 in the case of LDR_Val) it will call the output functions with a parameter that can be used to determine which of the 3 monitor functions is high, low or abnormal. 
+// If there are no issues the program will go back to the beginning for monitoring.
+
+//2 Output Functions that will either play a buzzer tone or flash the LED’s. 
+// Using the argument from the computational function it can produce a unique output based on which of the monitors is abnormal. 
+// The 2 outputs will continuously play until the issue is resolved (the user pushes a button). After which the program will go back to monitoring.
 
 //Pins
 
